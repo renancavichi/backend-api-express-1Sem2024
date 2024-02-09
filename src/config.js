e9f6efa54b46d82e989b1cacc3431fee3b61f814
Renan Cavichi <renancavichi@gmail.com>
2024-02-09 15:45:34 -0300

    setup server express basic

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const PORT = 3000
+export const HOST = 'http://localhost'
+
--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
-console.log("Olá mundoooo!")
+//const express = require('express')
+import express from 'express'
+import {PORT, HOST} from './config.js'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/produto', (req, res) => {
+    res.send('Hello World POST!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${HOST}:${PORT}`)
+})
