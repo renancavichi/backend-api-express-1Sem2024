16dbf00b443baaf9903cb9103d2cbd67bf7a74de
Renan Cavichi <renancavichi@gmail.com>
2024-02-16 16:02:15 -0300

    spread operator

--- revisao/aula-objeto.js
+++ revisao/aula-objeto.js
@@ -18,6 +18,21 @@ const user = {
     pets: [{nome: 'Tobias', raca: 'viralata'}, 'Gato', 'Peixe']
 } 
 
+const obj1 = {
+    prop1: 1,
+    prop2: 2
+}
 
+const obj2 = {
+    prop3: 3,
+    prop4: 4
+}
 
-console.log(user.pets[0].nome)
+const tudo = {...obj1, prop1: 5, ...obj2, prop4: 5}
+tudo.prop4 = 6
+delete tudo.prop1
+tudo.prop5 = 5
+tudo.prop6 = {teste1: '1', teste2: '2'}
+// const tudo = { prop1: 1, prop2: 2 }
+
+console.log(tudo)
