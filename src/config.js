c5543117e6665616504289000ac85b6eaa9986a1
Renan Cavichi <renancavichi@gmail.com>
2024-05-09 15:19:41 -0300

    update .env sample

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const PORT = process.env.PORT || 3000
+export const PORT = process.env.PORT || 3333
 export const HOST = process.env.HOST || 'http://localhost'
 
 export const SECRET_KEY = process.env.SECRET_KEY
